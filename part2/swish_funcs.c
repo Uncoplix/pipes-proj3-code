7ee19eee26a8623b47a42f3a4446aef53939acab
Uncoplix <uncoplix@gmail.com>
2025-11-03 05:58:07 +0000

    wrote most of run_pipelined_commands()

--- part2/swish_funcs.c
+++ part2/swish_funcs.c
@@ -26,11 +26,60 @@
  * Returns 0 on success or -1 on error.
  */
 int run_piped_command(strvec_t *tokens, int *pipes, int n_pipes, int in_idx, int out_idx) {
-    // TODO Complete this function's implementation
+    // TODO create implementation
     return 0;
 }
 
 int run_pipelined_commands(strvec_t *tokens) {
-    // TODO Complete this function's implementation
+    // create an array of pipes
+    int num_pipes = strvec_num_occurrences(tokens, "|");
+    int *pipes_fds = malloc(2 * num_pipes * sizeof(int));
+    for (int i = 0; i < num_pipes; i++) {
+        pipe(pipes_fds + 2 * i);
+    }
+
+    // fork children for each command
+    int num_commands = num_pipes + 1;
+    for (int i = 0; i < num_commands; i++) {
+        pid_t child_pid = fork();
+
+        if (child_pid < 0) {
+            perror("fork()");
+            free(pipes_fds);
+            return -1;
+        } else if (child_pid == 0) {
+            // child process
+
+            // determine I/O
+            int in_idx, out_idx = -1;
+            if (i == 0) {
+                // first command outputs to write end of first pipe
+                out_idx = 1;
+            } else if (i == num_commands - 1) {
+                // last command takes input from read end of last pipe
+                in_idx = 2 * (num_pipes - 1);
+            } else {
+                // middle commands
+                in_idx = 2 * (i - 1);    // read pipe connecting to last command
+                out_idx = 2 * i + 1;     // write to pipe connecting to next command
+            }
+
+            // TODO parse tokens for individual commands
+            strvec_t cmd_tokens;
+            strvec_init(&cmd_tokens);
+            // PARSING LOGIC HERE
+
+            // execute command
+            if (run_piped_command(&cmd_tokens, pipes_fds, num_pipes, in_idx, out_idx) == -1) {
+                fprintf(stderr, "run_piped_commands\n");
+            }
+
+            // should not reach this point if run_piped_commands() succeeds
+            free(pipes_fds);
+            return -1;
+        }
+    }
+
+    free(pipes_fds);
     return 0;
 }
